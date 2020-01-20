0a130457295a3326ad69618530502af4ebfea163
Dennis Haulund Nielsen <dennis.nielsen@akqa.com>
2020-01-20 16:18:35 +0100

    improvement(asset-config-webpack-plugin): support ttf fonts

--- packages/asset-config-webpack-plugin/src/config/production.config.ts
+++ packages/asset-config-webpack-plugin/src/config/production.config.ts
@@ -33,6 +33,7 @@ export = () => ({
                     "*.eot",
                     "*.woff",
                     "*.woff2",
+                    "*.ttf",
                     "*.html"
                 ]
             }
