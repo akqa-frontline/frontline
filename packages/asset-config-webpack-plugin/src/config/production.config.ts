02e47322292f704a9f068dc0cfc65907bc136d8c
Dennis Haulund Nielsen <dennis.nielsen@akqa.com>
2020-01-28 08:42:29 +0100

    improvement(webpack-config): dont copy any assets inside the module system (src) - all of this must

--- packages/asset-config-webpack-plugin/src/config/production.config.ts
+++ packages/asset-config-webpack-plugin/src/config/production.config.ts
@@ -5,38 +5,11 @@ export = () => ({
         rules: []
     },
     plugins: [
-        new CopyPlugin(
-            [
-                {
-                    from: "public/**/*",
-                    to: "",
-                    flatten: true
-                },
-                {
-                    from: "src/assets/**/*",
-                    to: "static/media/",
-                    flatten: true
-                }
-            ],
+        new CopyPlugin([
             {
-                ignore: [
-                    // All these files are handled by specific plugins
-                    "*.js",
-                    "*.jsx",
-                    "*.css",
-                    "*.scss",
-                    "*.gif",
-                    "*.jpg",
-                    "*.jpeg",
-                    "*.png",
-                    "*.svg",
-                    "*.eot",
-                    "*.woff",
-                    "*.woff2",
-                    "*.ttf",
-                    "*.html"
-                ]
+                from: "./public",
+                to: "./"
             }
-        )
+        ])
     ]
 });
